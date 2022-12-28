c482d7e3c9a3b3d87a23d1e406edaa337a7bff04
Dmitry Romenskiy (DevExpress) <romenskiy.dmitry@devexpress.com>
2022-12-28 17:38:07 +0400

    add source

---
+++ CS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace dxSample {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application { }
+}
+
---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Xpf.Bars;
+using DevExpress.Xpf.Core;
+using System;
+using System.Linq;
+using System.Windows;
+
+namespace dxSample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow {
+        Random r = new Random();
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void BarEditItem_EditValueChanged(object sender, RoutedEventArgs e) {
+            var editItem = sender as BarEditItem;
+            var value = Convert.ToInt32(editItem.EditValue);
+            Point[] points = new Point[value];
+
+            for (int i = 0; i < value; i++) {
+                points[i] = new Point() { X = r.NextDouble(), Y = r.Next(0, 2) == 0 ? 0 : 1 };
+            }
+
+            shape.ConnectionPoints = new DevExpress.Diagram.Core.DiagramPointCollection(points);
+        }
+    }
+}
